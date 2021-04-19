a4395fc83724f161c4210390c963ac7800591ced
Travis Tidwell <travis@form.io>
2021-04-19 12:47:34 -0500

    Updated build and version.

--- public/assets/scripts/choices.js
+++ public/assets/scripts/choices.js
@@ -1,4 +1,4 @@
-/*! choices.js v9.0.1 | © 2021 Josh Johnson | https://github.com/jshjohnson/Choices#readme */
+/*! @formio/choices.js v9.0.1 | © 2021 Josh Johnson | https://github.com/jshjohnson/Choices#readme */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
